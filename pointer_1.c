d0441266d9473e364a6147a28f71104f21708c20
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-28 10:43:19 +0530

    Create pointer_1.c

---
+++ pointer_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x=5;
+int y=6;
+int *p;
+int *q;
+p=&x;
+q=&y;
+*p=*q;
+printf("address of x:%u\n",p);
+printf("address of y:%u\n",q);
+printf("value of x:%d\n",*p);
+printf("value of y:%d\n",*q);
+return 0;
+}
