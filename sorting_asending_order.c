9b70889ea514c4601f16c65ad17fa4754d20279e
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-29 18:51:58 +0530

    Create sorting_asending_order.c

---
+++ sorting_asending_order.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,temp;
+printf("enter the value of n\n");
+scanf("%d",&n);
+int a[n];
+printf("enter the values of array\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t",a[i]);
+}
+return 0;
+}
